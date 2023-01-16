0ed337ecb16ea64ede38bdeba7fcb146f94b794e
Juan Quino <quinojuan@gmail.com>
2023-01-16 00:02:49 -0300

    eliminando imagenes... seguir

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import "./UploadImage.css";
+import axios from "axios";
 
 const UploadImage = () => {
   const [images, setImages] = useState([]);
   const [imageToRemove, setImageToRemove] = useState(null);
 
-  const handleRemoveImg = () => {};
+  const handleRemoveImg = (imgObj) => {
+    setImageToRemove(imgObj.public_id);
+    axios.delete(
+      `http://localhost:3001/nft/${imgObj.public_id}`
+        .then(() => {
+          setImageToRemove(null);
+          setImages((prev) =>
+            prev.filter((img) => img.public_id !== imgObj.public_id)
+          );
+        })
+        .catch((e) => console.log(e))
+    );
+  };
 
   const handleOpenWidget = async () => {
     var myWidget = window.cloudinary.createUploadWidget(
@@ -31,7 +44,10 @@ const UploadImage = () => {
       <div className="images-preview-container">
         {images.map((image) => (
           <div className="image-preview">
-            <img src={image.url} />
+            <img src={image.url} alt="img"/>
+            {imageToRemove !== image.public_id && (
+              <i className="fa fa-times-circle close-icon" onClick={() => handleRemoveImg(image)}></i>
+            )}
           </div>
         ))}
       </div>
