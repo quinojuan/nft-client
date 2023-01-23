32e6ed3132cfa072e3870ce329eb51fc69a5ee95
Juan Quino <quinojuan@gmail.com>
2023-01-23 00:57:44 -0300

    react-redux

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -7,8 +7,10 @@ const rootReducer = (state = initialState, action) => {
     case "GET_URL":
       return {
         ...state,
-        urlImage: action.payload
+        urlImage: action.payload,
       };
+    default:
+      return state;
   }
 };
 
